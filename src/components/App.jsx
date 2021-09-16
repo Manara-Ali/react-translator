66ae431e78bfef4a2c8c20023fc8889ffc07dd35
Manara Ali <manaraali64@gmail.com>
2021-09-16 03:48:24 -0400

    Initial set

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+// 1. IMPORT REACT
+import React from "react";
+
+// 2. ADDITIONAL IMPORTS
+
+// 3. CREATE A FUNCTION BASED COMPONENT
+const App = () => {
+  return (
+    <div>
+      <h1>App Component</h1>
+    </div>
+  );
+};
+
+// 4. EXPORT COMPONENT TO BE USED IN OTHER PARTS OF OUR APPLICATION
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+// 1. IMPORT REACT
+import React from "react";
+
+// 2. IMPORT REACTDOM
+import ReactDOM from "react-dom";
+
+// 3. ADDITIONAL IMPORTS
+import App from "./components/App";
+
+// 4. AUTO RELOAD
+if (module.hot) {
+  module.hot.accept();
+}
+
+// 5. RENDER COMPONENTS TO THE SCREEN
+ReactDOM.render(<App />, document.querySelector("#root"));
