8e3391a53405126470155f847f92beff52cd03e4
Manara Ali <manaraali64@gmail.com>
2021-09-16 08:42:37 -0400

    Add useEffect and make initial network request

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,16 +1,51 @@
 // 1. IMPORT REACT
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 // 2. ADDITIONAL IMPORTS
+// import axios from "axios";
+import api from "../googleApi/api";
 
 // 3. CREATE A FUNCTION BASED COMPONENT
-const TextInput = () => {
+const TextInput = (props) => {
   // Create a state variable to store user's inputs
   const [userInput, setUserInput] = useState("");
+
+  // Create a variable to store the selected language coming from props
+  let languageCode = props.selectedLanguage.code;
+
   // Create a helper function to keep track of what the user is typing
   const handleChange = (e) => {
     setUserInput(e.target.value);
   };
+
+  // Create an Effect to make API calls
+  useEffect(() => {
+    // Create a helper function to make asynchronous calls
+    const helperFunction = async () => {
+      try {
+        const response = await api.post(
+          "/",
+          {},
+          {
+            params: {
+              q: `${userInput}`,
+              target: `${languageCode}`,
+            },
+          }
+        );
+        console.log(response);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    // Now I need to invoke the helper function to make the request
+    if (userInput) {
+      helperFunction();
+    }
+  }, [languageCode, userInput]);
+
+  // Render component to the screen
   return (
     <div className="ui segment">
       <div className="ui form">
