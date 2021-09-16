066f506c5158e03639bca2606f13387ebe011a91
Manara Ali <manaraali64@gmail.com>
2021-09-16 07:50:56 -0400

    Create state variable to keep track of user input

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,16 +1,28 @@
 // 1. IMPORT REACT
-import React from "react";
+import React, { useState } from "react";
 
 // 2. ADDITIONAL IMPORTS
 
 // 3. CREATE A FUNCTION BASED COMPONENT
 const TextInput = () => {
+  // Create a state variable to store user's inputs
+  const [userInput, setUserInput] = useState("");
+  // Create a helper function to keep track of what the user is typing
+  const handleChange = (e) => {
+    setUserInput(e.target.value);
+  };
   return (
     <div className="ui segment">
       <div className="ui form">
         <div className="field">
           <label>Enter a Word</label>
-          <input type="text" />
+          <input
+            type="text"
+            onChange={(e) => {
+              return handleChange(e);
+            }}
+            value={userInput}
+          />
         </div>
       </div>
     </div>
